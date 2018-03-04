437e11cb6b1dc3a7e567d223306bc5ca32ca3b6b
Jonathan Daggerhart <jonathan@daggerhart.com>
2018-03-04 07:54:03 -0500

    another config subscribers to show off priorities

---
+++ modules/custom_events/src/EventSubscriber/AnotherConfigEventsSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\custom_events\EventSubscriber;
+
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class EntityTypeSubscriber.
+ *
+ * @package Drupal\custom_events\EventSubscriber
+ */
+class AnotherConfigEventsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   *   The event names to listen for, and the methods that should be executed.
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ConfigEvents::SAVE => ['configSave', 100],
+      ConfigEvents::DELETE => ['configDelete', -100],
+    ];
+  }
+
+  /**
+   * React to a config object being saved.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   Config crud event.
+   */
+  public function configSave(ConfigCrudEvent $event) {
+    $config = $event->getConfig();
+    drupal_set_message('(Another) Saved config: ' . $config->getName());
+  }
+
+  /**
+   * React to a config object being deleted.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   Config crud event.
+   */
+  public function configDelete(ConfigCrudEvent $event) {
+    $config = $event->getConfig();
+    drupal_set_message('(Another) Deleted config: ' . $config->getName());
+  }
+
+}
